da5377b68362afc9b9364525c1bb09d90f68f75a
Jan Iverson Eligio <janiverson.eligio.ctr@us.navy.mil>
2024-08-08 09:57:23 -1000

    Done.

--- AngularMashupTemplate.js
+++ AngularMashupTemplate.js
@@ -5,29 +5,60 @@
 /*
  *    Fill in host and port for Qlik engine
  */
-var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+var prefix = window.location.pathname.substr(
+    0,
+    window.location.pathname.toLowerCase().lastIndexOf('/extensions') + 1
+);
 var config = {
-	host: window.location.hostname,
-	prefix: prefix,
-	port: window.location.port,
-	isSecure: window.location.protocol === "https:"
+    host: window.location.hostname,
+    prefix: prefix,
+    port: window.location.port,
+    isSecure: window.location.protocol === 'https:',
 };
-require.config( {
-	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
-} );
+require.config({
+    baseUrl:
+        (config.isSecure ? 'https://' : 'http://') +
+        config.host +
+        (config.port ? ':' + config.port : '') +
+        config.prefix +
+        'resources',
+});
 
-require( ["js/qlik"], function ( qlik ) {
-	qlik.setOnError( function ( error ) {
-		$( '#popupText' ).append( error.message + "<br>" );
-		$( '#popup' ).fadeIn( 1000 );
-	} );
-	$( "#closePopup" ).click( function () {
-		$( '#popup' ).hide();
-	} );
+require(['js/qlik'], function (qlik) {
+    const angularMashupApp = angular.module('angularMashupApp', ['ngRoute']);
 
-	//callbacks -- inserted here --
-	//open apps -- inserted here --
-	//get objects -- inserted here --
-	//create cubes and lists -- inserted here --
+    angularMashupApp.config(function ($routeProvider) {
+        $routeProvider
+            .when('/hello-world', {
+                templateUrl: 'hello-world.html',
+                controller: 'HelloWorldController',
+            })
+            .otherwise({
+                controller: 'MainController',
+                templateUrl: 'index.html',
+            });
+    });
 
-} );
+    // Controllers
+    angularMashupApp.controller('MainController', [
+        '$scope',
+        function ($scope) {
+            $scope.title = 'Main';
+        },
+    ]);
+
+    angularMashupApp.controller('HelloWorldController', [
+        '$scope',
+        function ($scope) {
+            $scope.title = 'Main 2';
+            $scope.body = 'Hello World';
+        },
+    ]);
+
+    // Components
+    angular.module('angularMashupApp').component('navigation', {
+        templateUrl: 'navigation.html',
+    });
+
+    angular.bootstrap(document, ['angularMashupApp', 'qlik-angular']);
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# Angular Mashup Template
+
+## There are examples of
+
+-   How to bootstrap the mashup AngularJS
+-   How to use routes for dynamic views
+-   How to define components
